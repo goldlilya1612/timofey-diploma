fe070c704cc126e89796bfdd639983fd4ef065fa
goldlilya1612 <goldlilya1612@yandex.com>
2023-05-10 21:52:05 +0300

    add necessary files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,42 @@
+import './App.css';
+import { Route, Routes } from "react-router-dom";
+import RegistrationPage from "../RegistrationPage/RegistrationPage";
+import AuthorizationPage from "../AuthorizationPage/AuthorizationPage";
+import Main from "../Main/Main";
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
+import * as auth from "../../api/auth";
+
+function App() {
+    const handleRegister = (data) => {
+        return auth
+            .register(data)
+            .then(() => {
+                // dispatch(setIsLoading(true));
+                // handleLogin({ email, password }, setData, setErrors, resetForm);
+            })
+            .catch((err) => {
+
+            });
+    };
+
+  return (
+    <div className="App">
+      <Routes>
+          <Route
+              path="/"
+              element={
+                  <ProtectedRoute>
+                      <Main />
+                  </ProtectedRoute>
+              }
+          ></Route>
+        <Route path='/signup' element={(<RegistrationPage onRegister={handleRegister}/>)}>
+        </Route>
+        <Route path='/signin' element={(<AuthorizationPage />)}>
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
